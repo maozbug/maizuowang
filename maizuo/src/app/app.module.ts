ca0a15ddb666feba34197924d5169aee7838196d
maozb <maozbmail@163.com>
2017-10-10 14:02:51 +0800

    MAIZUOdianying
    修改left组件位置，产删除Commons。module

--- maizuo/src/app/app.module.ts
+++ maizuo/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonsModule } from './commons/commons.module';
+//import { CommonsModule } from './commons/commons.module';
 import { HomeModule } from './home/home.module';
+import { HeaderComponent } from './commons/header/header.component';
+import { LeftComponent } from './commons/left/left.component';
 
 import { ListsService } from './home/lists.service';
+import { ShowleftService } from './showleft.service';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,17 +17,18 @@ import {appRoutes} from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
-
+		HeaderComponent,
+		LeftComponent
   ],
   imports: [
     BrowserModule,
-    CommonsModule,
+//  CommonsModule,
     HomeModule,
     BrowserAnimationsModule,
     RouterModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [ListsService],
+  providers: [ListsService,ShowleftService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
